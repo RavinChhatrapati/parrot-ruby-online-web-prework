89d94472997696aefcf105d0c5634a3c0b817964
Ravin Chhatrapati <ravinchhatrapati95@gmail.com>
2018-11-27 02:21:49 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,7 @@
 # Create methoded `parrot` that outputs a given phrase and
 # returns the phrase
 require "pry"
-def parrot (phrase = "Squawk!")
+def parrparrot(phrase = "Squawk!")
   puts phrase
   phrase
 end
